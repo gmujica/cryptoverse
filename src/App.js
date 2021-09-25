7409a03d2d12a89f0cbadf75e969c75c82cd32ae
gmujica <gregorymujica1993@gmail.com>
2021-09-24 19:15:38 -0500

    navbar simple layout added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Switch, Router, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+//styles
+import './App.css'
+//components
+import { Navbar } from './components';
 
 const App = () => {
     return (
-        <div>
-            <h1>Cryptoverse</h1>
+        <div className='app'>
+            <div className='navbar'>
+                <Navbar />
+            </div>
+            <div className='main'>
+
+            </div>
+            <div className='footer'>
+
+            </div>
         </div>
     );
 }
